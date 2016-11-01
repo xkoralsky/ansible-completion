18cfe5f926d56e6cb1f628c76b4ede42f5abe5cf
Karol Ossowski <karol.ossowski@gmail.com>
2016-11-01 18:31:20 +0100

    ansible-vault upd. to ansible 2.1.1, enhance subcommand/option distinction

--- ansible-vault-completion.bash
+++ ansible-vault-completion.bash
@@ -1,19 +1,34 @@
-#!/bin/env bash
+#!/bin/env bash -e
 
 _ansible-vault() {
+    local comp_cword=$COMP_CWORD
     local current_word=${COMP_WORDS[COMP_CWORD]}
-    local previous_word=${COMP_WORDS[COMP_CWORD - 1]}
-    local options="create decrypt edit encrypt rekey view"
+    local previous_words=${COMP_WORDS[@]:1:COMP_CWORD-1}
+    local subcommands="create decrypt edit encrypt rekey"
+    local common_options="--vault-password-file --new-vault-password-file --ask-vault-pass -h --help -v --verbose --version"
+    local subcommand=""
 
-    case $previous_word in
-        create|decrypt|edit|encrypt|rekey|view)
-            options="--debug --vault-password-file -h --help"
+    for subc in $subcommands; do
+        if [[ ${previous_words[@]#$subc} != ${previous_words[@]} ]]; then
+            subcommand=$subc
+            break
+        fi
+    done
+
+    case $subcommand in
+        decrypt|encrypt)
             if [[ "$current_word" == -* ]]; then
+                local options="--output $common_options"
                 COMPREPLY=( $( compgen -W "$options" -- "$current_word" ) )
             fi
             ;;
+        create|edit|rekey)
+            if [[ "$current_word" == -* ]]; then
+                COMPREPLY=( $( compgen -W "$common_options" -- "$current_word" ) )
+            fi
+            ;;
         *)
-            COMPREPLY=( $( compgen -fdW "$options" -- "$current_word" ) )
+            COMPREPLY=( $( compgen -fdW "$subcommands" -- "$current_word" ) )
             ;;
     esac
 }
